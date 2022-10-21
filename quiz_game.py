d73faf6b3beebc7219c95c6bb3a753549928dce2
Gonenc Baris Bezik <112894114+gncbrs@users.noreply.github.com>
2022-10-21 18:11:54 +0300

    Update quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -12,37 +12,45 @@ if (start == "yes"):
     answer = input("What does CPU stand for? ").lower()
 
     if (answer == "central procress unit"):
+        time.sleep(1)
         print("Correct!")
         point += 1
 
     else:
+        time.sleep(1)
         print("Incorrect!")
     
     answer2 = input("What does GPU stand for? ").lower()
     
     if (answer2 == "graphics processing unit"):
+        time.sleep(1)
         print("Correct!")
         point += 1
 
     else:
+        time.sleep(1)
         print("Incorrect!")
 
     answer3 = input("What does RAM stand for? ").lower()
     
     if (answer3 == "random access memory"):
+        time.sleep(1)
         print("Correct!")
         point += 1
 
     else:
+        time.sleep(1)
         print("Incorrect!")
     
     answer3 = input("What does PSU  stand for? ").lower()
     
     if (answer3 == "power supply unit"):
+        time.sleep(1)
         print("Correct!")
         point += 1
 
     else:
+        time.sleep(1)
         print("Incorrect!")
 
 
