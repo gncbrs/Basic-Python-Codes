7b9dadfcf9276c3da99a6f57230ee913cb29b3b7
Gonenc Baris Bezik <112894114+gncbrs@users.noreply.github.com>
2022-11-05 10:56:03 +0300

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,14 @@
+import random
+
+chars = "qwertyuıopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!_-*?/\&,.()$£"
+
+while 1:
+    password_len = int(input("How many length would you like your password to be: "))
+    password_count = int(input("How many passwords would you like: "))
+
+    for x in range(0,password_count):
+        password = ""
+        for x in range(0,password_len):
+            password_char = random.choice(chars)
+            password = password + password_char
+        print("Here is your passwprd: ",password)
